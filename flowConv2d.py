2794658368d2ce4b0c74d54c5269c405dab4d92e
alex <kontakt@a-wels.de>
2023-02-16 14:48:01 +0000

    based on flownet2

--- flowConv2d.py
+++ flowConv2d.py
@@ -17,48 +17,56 @@ from utils.frame_utils import read_gen
 batch_size = 64
 import torch.nn as nn
 
+# Based on FlowNet2: https://arxiv.org/abs/1612.01925
 class OpticalFlow2D(nn.Module):
     def __init__(self):
         super(OpticalFlow2D, self).__init__()
         print("Building model...")
-        self.conv1 = nn.Conv2d(in_channels=2, out_channels=64, kernel_size=(3,3), stride=1, padding=1)
+        self.conv1 = nn.Conv2d(in_channels=2, out_channels=64, kernel_size=(7,7), stride=2, )
         self.bn1 = nn.BatchNorm2d(64)
         self.relu1 = nn.ReLU()
-        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
 
         print("Layers 1 done")
-        self.conv2 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(3,3), stride=1, padding=1)
+        self.conv2 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(5,5), stride=2)
         self.bn2 = nn.BatchNorm2d(128)
         self.relu2 = nn.ReLU()
-        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+
         print("Layers 2 done")
-        self.conv3 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=(3,3), stride=1, padding=1)
+        self.conv3 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=(5,5), stride=2)
         self.bn3 = nn.BatchNorm2d(256)
         self.relu3 = nn.ReLU()
-        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
-
         print("Layers 3 done")
-        self.fc1 = nn.Linear(256 * 43 * 15, 512)
+
+        self.conv4 = nn.Conv2d(in_channels=256, out_channels=512, kernel_size=(3,3), stride=1)
+        self.bn4 = nn.BatchNorm2d(512)
         self.relu4 = nn.ReLU()
         print("Layers 4 done")
-        self.fc2 = nn.Linear(512, 2 * 127 * 344)
+
+        self.conv5 = nn.Conv2d(in_channels=512, out_channels=512, kernel_size=(3,3), stride=1)
+        self.bn5 = nn.BatchNorm2d(512)
+        self.relu5 = nn.ReLU()
         print("Layers 5 done")
 
+        self.conv6 = nn.Conv2d(in_channels=512, out_channels=1024, kernel_size=(3,3), stride=1)
+        self.bn6 = nn.BatchNorm2d(1024)
+        self.relu6 = nn.ReLU()
+        print("Layers 6 done")
+
+        self.fc1 = nn.Linear(1024 * 34 * 7, 2048)
+        self.relu4 = nn.ReLU()
+        print("Layers 7 done")
+        self.fc2 = nn.Linear(2048, 2 * 127 * 344)
+        print("Layers 8 done")
+
     def forward(self, x):
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu1(x)
-        x = self.pool1(x)
+        x = self.relu1(self.bn1(self.conv1(x)))
+        x = self.relu2(self.bn2(self.conv2(x)))
+        x = self.relu3(self.bn3(self.conv3(x)))
+        x = self.relu4(self.bn4(self.conv4(x)))
+        x = self.relu5(self.bn5(self.conv5(x)))
+        x = self.relu6(self.bn6(self.conv6(x)))
 
-        x = self.conv2(x)
-        x = self.bn2(x)
-        x = self.relu2(x)
-        x = self.pool2(x)
-
-        x = self.conv3(x)
-        x = self.bn3(x)
-        x = self.relu3(x)
-        x = self.pool3(x)
         # flatten input from convolutional layers and pass through fc layers
         x = x.view(-1, x.shape[1] * x.shape[2] * x.shape[3])      
         x = self.fc1(x)
